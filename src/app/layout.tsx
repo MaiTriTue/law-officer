ad57a6b343aad190284c9edb1ff1900dae6b1639
Mai Trí Tuệ <93773841+MaiTriTue@users.noreply.github.com>
2023-06-07 22:14:52 +0700

    update message 8

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import Script from 'next/script';
 import { Inter } from 'next/font/google';
 import { Header, Footer } from '~/components/defaultLayout';
 import images from '~/assets/images';
@@ -22,6 +23,38 @@ export default function RootLayout({
     return (
         <html lang="vi">
             <body className={inter.className}>
+                <div id="fb-root"></div>
+
+                <div id="fb-customer-chat" className="fb-customerchat"></div>
+                <Script
+                    id="messenger-tag"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `{var chatbox = document.getElementById('fb-customer-chat');
+                            chatbox.setAttribute("page_id", "115713988209658");
+                            chatbox.setAttribute("attribution", "biz_inbox");}`,
+                    }}
+                ></Script>
+                <Script
+                    id="messenger-sdk"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `{window.fbAsyncInit = function() {
+                            FB.init({
+                            xfbml            : true,
+                            version          : 'v17.0'
+                            });
+                        };
+                    
+                        (function(d, s, id) {
+                            var js, fjs = d.getElementsByTagName(s)[0];
+                            if (d.getElementById(id)) return;
+                            js = d.createElement(s); js.id = id;
+                            js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+                            fjs.parentNode.insertBefore(js, fjs);
+                        }(document, 'script', 'facebook-jssdk'));}`,
+                    }}
+                ></Script>
                 <Header />
                 {children}
                 <Footer />
@@ -29,3 +62,21 @@ export default function RootLayout({
         </html>
     );
 }
+
+// export default function RootLayout({
+//     children,
+// }: {
+//     children: React.ReactNode;
+// }) {
+//     return (
+//         <html lang="vi">
+//             <body className={inter.className}>
+//                 {/* <div id="fb-root"></div>
+//                 <div id="fb-customer-chat" className="fb-customerchat"></div> */}
+//                 <Header />
+//                 {children}
+//                 <Footer />
+//             </body>
+//         </html>
+//     );
+// }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,9 @@
 import type { AppProps } from 'next/app';
-import Script from 'next/script';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
-import { useEffect } from 'react';
+
 config.autoAddCss = false;
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
 }
-//
