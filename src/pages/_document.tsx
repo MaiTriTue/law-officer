52cba0278bf8a144b3b787511666c555ef1fff9a
Mai Trí Tuệ <93773841+MaiTriTue@users.noreply.github.com>
2023-06-07 15:54:15 +0700

    update message 6

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,28 +16,28 @@ export default function Document() {
                     strategy="afterInteractive"
                     dangerouslySetInnerHTML={{
                         __html: `{var chatbox = document.getElementById('fb-customer-chat');
-            chatbox.setAttribute("page_id", "115713988209658");
-            chatbox.setAttribute("attribution", "biz_inbox");}`,
+                        chatbox.setAttribute("page_id", "115713988209658");
+                        chatbox.setAttribute("attribution", "biz_inbox");}`,
                     }}
                 ></Script>
                 <Script
                     id="messenger-sdk"
                     strategy="afterInteractive"
                     dangerouslySetInnerHTML={{
-                        __html: `{ window.fbAsyncInit = function() {
-                FB.init({
-                  xfbml            : true,
-                  version          : 'v17.0'
-                });
-              };
-        
-              (function(d, s, id) {
-                var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id)) return;
-                js = d.createElement(s); js.id = id;
-                js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
-                fjs.parentNode.insertBefore(js, fjs);
-              }(document, 'script', 'facebook-jssdk'));}`,
+                        __html: `{window.fbAsyncInit = function() {
+                            FB.init({
+                              xfbml            : true,
+                              version          : 'v17.0'
+                            });
+                          };
+                    
+                          (function(d, s, id) {
+                            var js, fjs = d.getElementsByTagName(s)[0];
+                            if (d.getElementById(id)) return;
+                            js = d.createElement(s); js.id = id;
+                            js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+                            fjs.parentNode.insertBefore(js, fjs);
+                          }(document, 'script', 'facebook-jssdk'));}`,
                     }}
                 ></Script>
                 <Main />
@@ -48,7 +48,7 @@ export default function Document() {
 }
 
 // <!-- Messenger Plugin chat Code -->
-//     <div id="fb-root"></div>
+// <div id="fb-root"></div>
 
 //     <!-- Your Plugin chat code -->
 //     <div id="fb-customer-chat" class="fb-customerchat">
